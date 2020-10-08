4389d7bf022237415d02f57b141162bc5df44779
bogyaanna2003 <47587998+bogyaanna2003@users.noreply.github.com>
2020-10-08 10:47:53 +0300

    Add files via upload

---
+++ foatloba 1esek, foatlo alatt2esek, foatlo folott 0sok/main.cpp
@@ -0,0 +1,38 @@
+#include <iostream>
+
+using namespace std;
+
+int main()
+{
+    int sor, oszlop;
+    int n, m[100][100];
+    cout << "Kerem a matrix meretet:";
+    cin >> n;
+    for(sor=0; sor<n; sor++){
+        for(oszlop = 0; oszlop < n; oszlop++){
+            cout<< "m[" << sor << "][" << oszlop << "]=";
+            cin >> m[sor][oszlop];
+            if(sor>oszlop){
+                 m[sor][oszlop]=2;
+            }
+            if(sor==oszlop){
+                m[sor][oszlop]=1;
+            }
+            if(sor<oszlop){
+                 m[sor][oszlop]=0;
+            }
+
+        }
+    }
+    for(sor=0; sor<n; sor++){
+        for(oszlop = 0; oszlop < n; oszlop++){
+            cout << m[sor][oszlop] << " ";
+        }
+        cout << endl;
+
+
+        }
+
+
+    return 0;
+}
