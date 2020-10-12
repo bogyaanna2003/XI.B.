afc3b47af325ff6b0dfc402d22b93f9335c54c82
bogyaanna2003 <47587998+bogyaanna2003@users.noreply.github.com>
2020-10-12 17:22:21 +0300

    Add files via upload

---
+++ E es D osszegenek lnko, K es NY osszegenek lkkt/main.cpp
@@ -0,0 +1,54 @@
+#include <iostream>
+#include <fstream>
+
+using namespace std;
+
+int main()
+{
+    int  c, n, i, j, E=0, K=0, D=0, NY=0;
+    int m[n][n];
+    ifstream in("input.txt");
+    in >> n;
+    for(i=0; i<n; i++)
+    {
+        for(j=0; j<n; j++)
+        {
+            in >> m[i][j];
+            if(i<j && i+j < n-1)
+            {
+                E = E + m[i][j];
+            }
+            if(i<j && i+j > n-1)
+            {
+                K = K + m[i][j];
+            }
+            if(i>j && i+j > n-1)
+            {
+                D = D + m[i][j];
+            }
+            if(i>j && i+j < n-1)
+            {
+                NY = NY + m[i][j];
+            }
+        }
+        }
+        while(E!=D){
+            if(E>D){
+               E=E-D;
+               }else{
+               D=D-E;
+               }
+               }
+               c= K*NY;
+       while(K!=NY){
+            if(K>NY){
+               K=K-NY;
+               }else{
+               NY=NY-K;
+               }
+               }
+
+    cout << "E es D haromszogek osszegenek lnkoja=" << E << endl;
+    cout << "K es NY haromszogek osszegenek lkktje=" << c/K << endl;
+    return 0;
+}
