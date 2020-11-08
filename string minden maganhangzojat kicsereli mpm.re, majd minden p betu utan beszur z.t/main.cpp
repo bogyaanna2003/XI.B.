8a1642d09bdcb687c569702313494d198043059f
bogyaanna2003 <47587998+bogyaanna2003@users.noreply.github.com>
2020-11-08 22:10:04 +0200

    Add files via upload

--- string minden maganhangzojat kicsereli mpm.re, majd minden p betu utan beszur z.t/main.cpp
+++ string minden maganhangzojat kicsereli mpm.re, majd minden p betu utan beszur z.t/main.cpp
@@ -5,23 +5,25 @@ using namespace std;
 
 int main()
 {
-    int found;
-  string str,str1="aeiou",str2="mpm",str3="p", str4="z";
-  cout <<"kerem a szoveget:";
-  getline (cin,str);
 
-  for(int i=0;i<= str1.size();i++){
-   found = str.find(str1[i]);
-   while(found!=string::npos){
-         str=str.replace(found,1,str2);
-        found = str.find(str1[i],found+3);
+    string str,str1="aeiou",str2="mpm",str3="p", str4="Z";
+    cout << "kerem a szoveget:";
+    getline (cin,str);
+    for(int i=0; i<=str1.size(); i++)
+    {
+       int found = str.find(str1[i]);
+        while(found!=string::npos)
+        {
+            str = str.replace(found,1,str2);
+            found = str.find(str1[i],found+1);
+        }
     }
-}
-   found = str.find(str3);
-  while(found!=string::npos){
-  str.insert(found+1,str4);
-  found = str.find(str3,found+1);
-    cout << str;
-  }
+     int found = str.find(str3);
+    while(found!=string::npos)
+    {
+        str.insert(found+1,str4);
+        found = str.find(str3,found+1);
+    }
+      cout<<str;
     return 0;
 }
