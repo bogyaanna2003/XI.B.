3d47555dd30ee2eab43ad2e9a4ce535980f09289
bogyaanna2003 <47587998+bogyaanna2003@users.noreply.github.com>
2021-01-11 09:33:28 +0200

    Add files via upload

--- rekunziv, binaris/main.cpp
+++ rekunziv, binaris/main.cpp
@@ -1,24 +1,32 @@
 #include <iostream>
 
 using namespace std;
-int binaris(int t[], int bal, int jobb, int keres){
-    if (bal==jobb){
-        if(t[bal]==keres){
-        return bal;
+
+int binaris(int v[], int balindex, int jobbindex, int szam)
+{
+    if (balindex <= jobbindex)
+    {
+        int kozep = (balindex + jobbindex) / 2;
+        if (v[kozep] == szam)
+        {
+            return kozep;
         }
-        return -1;
+        if (v[kozep] > szam)
+        {
+            return binaris(v, balindex, kozep-1, szam);
+        }
+
+        if (v[kozep] < szam)
+        {
+            return binaris(v, kozep+1, jobbindex, szam);
+        }
     }
-    int kozep=(bal+jobb)/2;
-    if (t[kozep]<keres){return binaris(t, kozep+1, jobb, keres);
-    }
-    if(t[kozep]==keres){
-    return kozep;
-    }
-    return binaris(t, bal, kozep-1, keres);
+    return -1;
 }
 int main()
 {
-    int t[10]={1,2,3,4,5,6,7,8,9,10};
-    cout<<binaris(t, 0, 9, 3);
+    int v[] = {1, 2, 3, 5, 6, 7, 8, 9, 10};
+
+    cout << binaris(v, 0, 10, 4) << endl;
     return 0;
 }
